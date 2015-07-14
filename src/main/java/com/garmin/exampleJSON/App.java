fed553a7740c7c4090df80f509297450fe5d66d1
ionellechintan <lechintan.ionel@gmail.com>
2015-07-14 16:13:49 +0300

    tema de acasa

--- src/main/java/com/garmin/exampleJSON/App.java
+++ src/main/java/com/garmin/exampleJSON/App.java
@@ -21,10 +21,7 @@ class App {
 			JsonNode rootNode = mapper.readValue(new File("JSON.txt"),
 					JsonNode.class);
 			JsonParser jParser= mapper.getJsonFactory().createJsonParser(new File("JSON.txt"));
-		while(jParser.nextToken()!=null){
-				
-				System.out.println(jParser.getCurrentName());
-			}
+			
 			rootNode=mapper.readTree(jParser);
 			
 			
